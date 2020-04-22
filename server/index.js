ba9875fca72fc6da4127392691b97f2a0dafd89c
rudenick0309 <rudenick0309@gmail.com>
2020-04-22 17:01:01 +0900

    express

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
