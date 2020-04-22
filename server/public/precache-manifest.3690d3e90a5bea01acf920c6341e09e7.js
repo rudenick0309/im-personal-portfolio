3e10e48c7442c277fea33b6703bfb27fa927d415
rudenick0309 <rudenick0309@gmail.com>
2020-04-22 16:47:44 +0900

    build and server

---
+++ server/public/precache-manifest.3690d3e90a5bea01acf920c6341e09e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ada9535c3d8f9bf4f7af0875e59c58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "847cab1c13dff3e51e3d",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "7ac031e22626e650c6c3",
+    "url": "/static/js/2.bdca552c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bdca552c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847cab1c13dff3e51e3d",
+    "url": "/static/js/main.f90bfcb0.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
